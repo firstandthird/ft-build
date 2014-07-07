98b4ce554bd8275ec7864102866a99b05a004773
Greg Allen <me@jga.me>
2014-07-07 14:42:07 -0700

    [browser] initial work on refactoring

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-module.exports = function(grunt, obj) {
+var checkRequires = require('./lib/check-requires');
+var updateNotifier = require('update-notifier');
+var path = require('path');
+
+module.exports = function(type, grunt, obj) {
   //update notifier
-  var updateNotifier = require('update-notifier');
   var notifier = updateNotifier();
 
   if (notifier.update) {
-      notifier.notify();
+    notifier.notify();
   }
 
-  var path = require('path');
+  var types = {
+    browser: require('./lib/browser')
+  };
 
-  var libName = obj.filename || obj.name;
-  var fullLibName = obj.fullFilename || obj.name+'.full';
 
-  require('load-grunt-config')(grunt, {
-    configPath: path.join(__dirname, 'grunt'),
-    config: {
-      name: obj.name,
-      info: grunt.file.readJSON('bower.json'),
-      libName: libName,
-      fullLibName: fullLibName,
-      bowerExclude: obj.bowerExclude || [],
-      fatjsPath: __dirname,
-      jshintStylish: require('jshint-stylish')
-    },
-    loadGruntTasks: false
+  var requires = types[type].requires;
+  var missingReqs = checkRequires(path.resolve(process.cwd(), 'node_modules'), requires);
+
+  if (missingReqs.length != 0) {
+    console.log('Not all required modules are installed, please run this:');
+    var cmd = 'npm install --save-dev '+ missingReqs.join(' ');
+    console.log(cmd);
+    process.exit(0);
+  }
+
+  types[type].run(grunt, obj, function() {
+
   });
 
-  grunt.file.expand(path.join(__dirname, 'node_modules/grunt-*/tasks')).forEach(grunt.loadTasks);
-
-  if (obj.full === false) {
-    grunt.registerTask('scripts', ['scriptLib']);
-  } else {
-    grunt.registerTask('scripts', ['scriptLib', 'scriptFull']);
-  }
 
 };
